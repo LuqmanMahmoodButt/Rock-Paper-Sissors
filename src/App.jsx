4e6cdeb73d6af7d2ac27c55339aeb63cc53ff44c
Luqman B <luqmanmbutt.dev@gmail.com>
2024-06-04 17:17:50 +0100

    function and jsx is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,69 @@
 import './App.css'
+import React from 'react'
+
 
 function App() {
+  
+
+  const [playerChoice, setPlayerChoice] = React.useState("") 
+  const [computerChoice, setComputerChoice] = React.useState(Math.floor(Math.random() * 3))
+
+  const choices = ["Rock", "Paper", "Scissors"]
+
+
+
+  function handleClick(e) {
+
+    setPlayerChoice(e.target.innerText)
+    const randomChoice = Math.floor (Math.random() * choices.length)
+    setComputerChoice(choices[randomChoice])
+    gameResult()
+
+  }
+  
+  console.log("player", playerChoice);
+  console.log("computer", computerChoice)
+  
+  
+  const gameResult = () => {
+    
+    if (playerChoice === 'Rock' && computerChoice === "Scissors")  {
+      return "Player Wins"
+    } else if (playerChoice === 'Rock' && computerChoice === "Paper"){
+      return "Computer Wins"
+    } else if (playerChoice === 'Scissors' && computerChoice === "Paper"){
+      return "Player Wins"
+    } else if (playerChoice === 'Scissors' && computerChoice === "Rock") {
+      return "Computer Wins"
+    } else if (playerChoice === 'Paper' && computerChoice === "Rock") {
+      return "Player Wins"
+    } else if (playerChoice === 'Paper' && computerChoice === "Scissors") {
+      return "Computer Wins"
+    } else if (playerChoice && computerChoice) {
+      return "It's a Draw"
+    } else {
+      return "Click to Play"
+    }
+  }
+
+    function handleReset() {
+      setPlayerChoice()
+      setComputerChoice()
+    }
+  
+  
   return (
+    
     <div className="game">
-      <p>Player 1 chose: <span>{"PLAYER CHOICE GOES HERE"}</span></p>
-      <p>Player 2 chose: <span>{"COMPUTER CHOICE HERE"}</span></p>
-      <p className="result">{"RESULT GOES HERE"}</p>
-      <button>Rock</button>
-      <button>Paper</button>
-      <button>Scissors</button>
-      <button id="reset">reset</button>
+      <p>Player  Chose: <span>{playerChoice}</span></p>
+      <p>Computer Chose: <span>{computerChoice}</span></p>
+      <p className="result">{gameResult()}</p>
+      <button onClick={handleClick}>Rock</button>
+      <button onClick={handleClick}>Paper</button>
+      <button onClick={handleClick}>Scissors</button>
+      <button onClick={handleReset} id="reset">Reset</button>
     </div>
+
   )
 }
 
